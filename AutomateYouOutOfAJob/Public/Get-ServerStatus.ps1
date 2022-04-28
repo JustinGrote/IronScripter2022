e98c9797048dad5ba4ec791958a738841fdd2108
Justin Grote <JustinGrote@users.noreply.github.com>
2022-04-28 14:49:58 -0700

    Add Remoting

--- AutomateYouOutOfAJob/Public/Get-ServerStatus.ps1
+++ AutomateYouOutOfAJob/Public/Get-ServerStatus.ps1
@@ -1,24 +1,33 @@
 function Get-ServerStatus {
     <#
     .SYNOPSIS
-    Monitors the performance of the server
+    Monitors the performance of the server in real time
     #>
     [CmdletBinding()]
     param(
-
+        #The name of the computer to connect to, defaults to localhost
         [String]$ComputerName,
+        [PSCredential]$Credential,
         [int]$Lines = 10,
         [int]$RefreshSec = 1
     )
 
     while ($true) {
         Clear-Host
+        $icmParams = @{}
 
-        Get-Process
-        | Select-Object -First 10
-        | Sort-Object CPU
-        | Format-Table -AutoSize
+        if ($ComputerName) { $icmParams.ComputerName = $ComputerName }
+        if ($Credential) { $icmParams.Credential = $Credential }
 
-        Start-Sleep $refreshSec
+        Invoke-Command @icmParams {
+            Get-Process
+            | Select-Object -First 10
+            | Sort-Object CPU
+            | Format-Table -AutoSize
+
+            Start-Sleep $refreshSec
+
+        }
+
     }
 }
