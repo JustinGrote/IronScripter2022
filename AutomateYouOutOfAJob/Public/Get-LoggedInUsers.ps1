93f2d4b69cf8d256b0e84845887b382189265c95
Justin Grote <JustinGrote@users.noreply.github.com>
2022-04-28 15:04:46 -0700

    GetLoggedInUsers

--- AutomateYouOutOfAJob/Public/Get-LoggedInUsers.ps1
+++ AutomateYouOutOfAJob/Public/Get-LoggedInUsers.ps1
@@ -1,17 +1,14 @@
 function Get-ServerStatus {
     <#
     .SYNOPSIS
-    Monitors the performance of the server in real time
+    Fetches the currently logged in users of the server
     #>
     [CmdletBinding()]
     param(
         #The name of the computer to connect to, defaults to localhost
         [String][Alias('Hostname')]$ComputerName,
         #The credential to use for authentication
-        [PSCredential]$Credential,
-        #How many lines you wish to have displayed for Top
-        [int]$Lines = 10,
-        [int]$RefreshSec = 1
+        [PSCredential]$Credential
     )
 
     $icmParams = @{}
@@ -21,15 +18,7 @@ function Get-ServerStatus {
     if ($Credential) { $icmParams.Credential = $Credential }
 
     Invoke-Command @icmParams {
-        while ($true) {
-            Clear-Host
-
-            Get-Process
-            | Select-Object -First 10
-            | Sort-Object CPU
-            | Format-Table -AutoSize
-
-            Start-Sleep $refreshSec
-        }
+        #TODO: ANYTHING BUT THIS!
+        quser
     }
 }
